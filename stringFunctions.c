fc2f352c03eff15541a9a37a31f59aee6c7a2e7e
Simon Muia <simonmuia38@gmail.com>
2023-05-19 23:08:55 +0300

    cleaned to betty style format

--- stringFunctions.c
+++ stringFunctions.c
@@ -11,18 +11,20 @@ int _stcmp(const char *s1, const char *s2)
 {
 	if (s1 == NULL || s2 == NULL)
 		return (0);
-	while (*s1 && (*s1 == *s2)) {
+	while (*s1 && (*s1 == *s2))
+	{
 		s1++;
 		s2++;
 	}
-	return *(unsigned char *)s1 - *(unsigned char *)s2;
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
+
 /**
  * _putchar - function that writes a char to stdout
  * @c: character to be printed
- * Return: returns the number of bytes written 
+ * Return: returns the number of bytes written
  */
 int _putchar(int c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }
