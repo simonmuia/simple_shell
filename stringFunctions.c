7c3946750d8ea22112f2819d066da7a0692c0a69
Johnmaestro <johnmariita421@gmail.com>
2023-05-19 23:03:39 +0300

    added a putchar function

--- stringFunctions.c
+++ stringFunctions.c
@@ -17,3 +17,12 @@ int _stcmp(const char *s1, const char *s2)
 	}
 	return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
+/**
+ * _putchar - function that writes a char to stdout
+ * @c: character to be printed
+ * Return: returns the number of bytes written 
+ */
+int _putchar(int c)
+{
+	return(write(1, &c, 1));
+}
