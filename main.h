61a70ad5d6b8b821028ef1bf3c6bf7f59a9f78cc
Simon Muia <simonmuia38@gmail.com>
2023-05-19 22:04:27 +0300

    cleaned up signal library import

--- main.h
+++ main.h
@@ -6,6 +6,9 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
+
+
 #define MAX_ARGS 100
 char **input_to_cmd(char *line);
 void execute(char *filename, char **cmd, int line);
--- myshell.c
+++ myshell.c
@@ -1,4 +1,3 @@
-#include <signal.h>
 #include "main.h"
 
 /**
