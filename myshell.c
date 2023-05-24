866216a335ccde59737f35b3ce3f6bcc24256c2f
Johnmaestro <johnmariita421@gmail.com>
2023-05-24 11:00:41 +0300

    modified some code

--- myshell.c
+++ myshell.c
@@ -8,7 +8,7 @@
 */
 int main(int argc __attribute__((unused)), char **argv, char **env)
 {
-	char *line, **cmd_args;
+	char *line = "", **cmd_args;
 	size_t length;
 	int string_length, line_count = 0;
 	char *filename =  argv[0];
@@ -22,6 +22,11 @@ int main(int argc __attribute__((unused)), char **argv, char **env)
 		write(1, "prompt> ", 8);
 		if (_getline(&line, &length, stdin) > 0)
 		{
+			if (_strcmp(line, "") == 0)
+			{
+				free(line);
+				continue;
+			}
 			while (line[string_length] != '\0')
 				string_length++;
 			line[string_length - 1] = '\0';
@@ -33,15 +38,8 @@ int main(int argc __attribute__((unused)), char **argv, char **env)
 				cmd_args = input_to_cmd(line);
 				execute(filename, cmd_args, line_count);
 			}
+			free(cmd_args);
 		}
-		else
-		{
-			if (_strcmp(line, "") == 0)
-				free(line);
-			_putchar(10);
-			break;
-		}
-		free(cmd_args);
 		if (!isatty(0))
 			break;
 	}
